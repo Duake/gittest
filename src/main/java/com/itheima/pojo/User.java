5ecc94664b81ce487b8ade3875f09ee6be81bb43
root <123456@qq.com>
2019-12-19 17:31:05 +0800

    firstcommit

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,27 @@
+package com.itheima.pojo;
+
+/**
+ * @description:
+ * @author: Duake
+ * @time: 2019/12/19 17:22
+ */
+public class User {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
